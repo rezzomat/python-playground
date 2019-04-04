072b2ad791bc4674ca912bb165e85296becb2ff6
rezzomat <rezzomat@students.zhaw.ch>
2019-04-04 13:19:39 +0200

    popper error fixed, added sidebar, added login

--- server.py
+++ server.py
@@ -1,34 +1,58 @@
 from flask import Flask
 from flask import render_template
+from flask import session
+from flask import redirect
+
+from flask_login import LoginManager
+from flask_login import UserMixin
+from flask_login import login_user
+from flask_login import logout_user
+from flask_login import current_user
+
 from flask_nav import Nav
 from flask_nav import register_renderer
 from flask_nav.elements import *
+
 from flask_bootstrap import Bootstrap
 from flask_bootstrap.nav import BootstrapRenderer
-from dominate import tags
-from hashlib import sha1
+
+from flask_sqlalchemy import SQLAlchemy
+
+from requests_oauthlib import OAuth2Session
+
+import os
 
 
 app = Flask(__name__)
 Bootstrap(app)
 app.config["SECRET_KEY"] = 'supersecret'
+db = SQLAlchemy(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'discord_login'
 
 nav = Nav()
 
 
+class User(UserMixin, db.Model):
+    __tablename__ = 'LoginUser'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(256), nullable=False)
+    discriminator = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, discord_id, username, discriminator):
+        self.id = int(discord_id)
+        self.username = username
+        self.discriminator = discriminator
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
 class Subview(View):
     pass
-
-
-class Banner(Navbar):
-
-    def __init__(self, text, endpoint, **kwargs):
-        super(text)
-        self.endpoint = endpoint
-        self.url_for_kwargs = kwargs
-
-    def get_url(self):
-        return url_for(self.endpoint, **self.url_for_kwargs)
 
 
 class MyNavRenderer(BootstrapRenderer):
@@ -84,17 +108,28 @@ class MyNavRenderer(BootstrapRenderer):
 
 @nav.navigation()
 def my_navbar():
-    return Navbar(Link('Python Playground', '#'),
-                  View('Home', 'index'),
-                  View('Calendar', 'index'),
-                  View('Configuration', 'index'),
-                  Subgroup('Auth',
-                           Subview('Login', 'index'),
-                           Separator(),
-                           Subview('Logout', 'index')
-                           )
-                  )
-
+    if current_user.is_authenticated:
+        return Navbar(Link('Python Playground', '#'),
+                      View('Home', 'index'),
+                      View('Calendar', 'index'),
+                      View('Configuration', 'index'),
+                      Subgroup('Auth',
+                               Text(current_user.username),
+                               Separator(),
+                               Subview('Logout', 'discord_logout')
+                               )
+                      )
+    else:
+        return Navbar(Link('Python Playground', '#'),
+                      View('Home', 'index'),
+                      View('Calendar', 'index'),
+                      View('Configuration', 'index'),
+                      Subgroup('Auth',
+                               Subview('Login', 'discord_login'),
+                               Separator(),
+                               Subview('Logout', 'index')
+                               )
+                      )
 
 
 @app.route("/")
@@ -102,9 +137,87 @@ def index():
     return render_template('index.html')
 
 
+# =====================================================
+# Auth
+# =====================================================
+
+OAUTH2_CLIENT_ID = 350502671137898498
+OAUTH2_REDIRECT_URI = 'http://localhost:5000/discord/callback'
+OAUTH2_CLIENT_SECRET = 'dyIqCZsCf7jSSbYNppa96CWBo2Sp7dtg'
+API_BASE_URL = 'https://discordapp.com/api'
+TOKEN_URL = API_BASE_URL + '/oauth2/token'
+AUTHORIZATION_BASE_URL = API_BASE_URL + '/oauth2/authorize'
+
+
+def token_updater(token):
+    session['oauth2_token'] = token
+
+
+def make_session(token=None, state=None, scope=None):
+    return OAuth2Session(
+        client_id=OAUTH2_CLIENT_ID,
+        token=token,
+        state=state,
+        scope=scope,
+        redirect_uri=OAUTH2_REDIRECT_URI,
+        auto_refresh_kwargs={
+            'client_id': OAUTH2_CLIENT_ID,
+            'client_secret': OAUTH2_CLIENT_SECRET
+        },
+        auto_refresh_url=TOKEN_URL,
+        token_updater=token_updater
+    )
+
+
+@app.route('/discord/login')
+def discord_login():
+    scope = request.args.get('scope', 'identify guilds')
+    discord = make_session(scope=scope.split(' '))
+    auth_url, state = discord.authorization_url(AUTHORIZATION_BASE_URL)
+    session['oauth2_token'] = state
+    return redirect(auth_url)
+
+
+@app.route('/discord/callback')
+def discord_callback():
+    if request.values.get('error'):
+        return request.values['error']
+
+    discord = make_session(state=session.get('oauth2_state'))
+    token = discord.fetch_token(
+        TOKEN_URL,
+        client_secret=OAUTH2_CLIENT_SECRET,
+        authorization_response=request.url
+    )
+    user = discord.get(API_BASE_URL + '/users/@me').json()
+    user_id = user['id']
+
+    loginuser = User.query.filter_by(id=user_id).first()
+    if not loginuser:
+        username = user['username']
+        discriminator = user['discriminator']
+        loginuser = User(user['id'], user['username'], user['discriminator'])
+        db.session.add(loginuser)
+        db.session.commit()
+
+    login_user(loginuser, True)
+    session['username'] = user['username']
+    session['oauth2_token'] = token
+
+    return redirect(url_for('index'))
+
+
+@app.route('/discord/logout')
+def discord_logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
 register_renderer(app, 'navbar', MyNavRenderer)
 nav.init_app(app)
 
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
